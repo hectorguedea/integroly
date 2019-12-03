fac0e55f80239ac5c9b7e685602a4dfbc3355d42
Héctor Guedea <hectorguedea@gmail.com>
2019-12-03 08:54:32 -0600

    Added Tokenizer

--- wordnet/index.js
+++ wordnet/index.js
@@ -2,6 +2,17 @@ const natural = require("natural");
 const wordnet = new natural.WordNet();
 words = process.argv.slice(2);
 
+var classifier = new natural.BayesClassifier();
+var wTokenizer = new natural.WordTokenizer();
+var aTokenizer = new natural.AggressiveTokenizerEs(); /* Español */
+let extract = "Héctor Guedea votó por AMLO para la Presidencia de México 2019";
+
+console.log("AgressiveTokenizer", aTokenizer.tokenize(extract));
+console.log("WordTokenizer", wTokenizer.tokenize(extract));
+
+console.log(natural.PorterStemmerEs.stem("votaciones"));
+
+console.log("------------------------------");
 console.log("Número de palabras", words.length);
 
 words.forEach(word => {
